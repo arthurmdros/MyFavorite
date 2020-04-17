d194c25c35856eee142d8b9ceb7dccbef12cc179
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-16 22:57:35 -0300

    Adicionando template inicial da screen main

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,21 +1,51 @@
 import React from 'react';
+import { SimpleLineIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 
+import logoImg from '../../assets/logo_2X.png';
 import styles from './styles';
 
 export default function Main() {
     const navigation = useNavigation();
 
     function navigateToLogin(){
+        alert('Saindo...');
         navigation.navigate('Login');
     }
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.button} onPress={navigateToLogin}>
-                <Text style={styles.buttonText}>Logout</Text>
-            </TouchableOpacity>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <TouchableOpacity style={styles.logout} onPress={navigateToLogin}>
+                    <SimpleLineIcons name='logout' size={25} color={'#FFF'}/>
+                    <Text style={styles.logoutText}>Sair</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.headerTitle}>
+                <Text style={styles.title}> Bem-Vindo, fulano!</Text>
+                <Text style={styles.description}> 
+                Crie sua lista de séries, filmes, documentários favoritos. 
+                Adicione comentários sobre ela ou adicione para lembrar-se de assistir depois.
+                </Text>
+            </View>
+
+            <View style={styles.favoritesList}>                
+                    <TouchableOpacity style={styles.button} onPress={() => alert('Cadastrar')}>
+                    <Text style={styles.buttonText}>Cadastrar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => alert('Atualizar')}>
+                    <Text style={styles.buttonText}>Atualizar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => alert('Deletar')}>
+                    <Text style={styles.buttonText}>Deletar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => alert('Listar')}>
+                    <Text style={styles.buttonText}>Listar</Text>
+                    </TouchableOpacity>                               
+            </View>
         </View>
     );
 }
--- frontend/src/pages/Main/styles.js
+++ frontend/src/pages/Main/styles.js
@@ -1,14 +1,60 @@
 import { StyleSheet } from "react-native";
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#6c4fbb',
+      paddingTop: Constants.statusBarHeight + 10,      
+    },
+
+    header:{
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+
+    logout: {
+      flexDirection: 'row',
+      paddingHorizontal: 24,
+      marginTop: 30,
+    },
+
+    logoutText: {
+      color: '#FFF',
+      fontSize: 15,
+      fontWeight: 'bold',
+      marginLeft: 15,
+      marginTop: 5,
+    },
+
+    headerTitle:{
+      paddingHorizontal: 12,
+      marginTop: 10,
+    },
+
+    title:{
+      fontSize: 20,
+      marginBottom: 16,
+      marginTop: 20,
+      color: '#FFF',
+      fontWeight: 'bold',
+    },
+
+    description: {
+      fontSize: 16,
+      lineHeight: 24,
+      color: '#FFF',
+    },
+
+    favoritesList:{
+      marginTop: 30,
+      flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
     },
 
     button:{
+        marginTop: 10,
         backgroundColor: '#FFF',
         borderRadius: 8,
         height: 40,

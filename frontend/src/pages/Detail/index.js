513eb27231d850ebc1adab351bdc4303350ce9ae
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-20 01:25:07 -0300

    Adicionando navegação entre páginas e renderização de item

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { Image, TouchableOpacity, View, Text } from 'react-native';
+import { Feather, AntDesign } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
+import logoImg from '../../assets/logo_2X.png';
 import styles from './styles';
 
 export default function Detail(){
+    const route = useRoute();
+    const navigation = useNavigation();
+
+    const favorite = route.params;
+
+    function navigateToEdit(){
+        navigation.navigate('Update');
+    }
+
+    function navigateToMain(){
+        navigation.navigate('Main');
+    }
+
     return(
         <View style={styles.container}>
-            <Text style={styles.text}>Detail</Text>
+
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <TouchableOpacity style={styles.return} onPress={navigateToMain}>
+                    <Feather name='arrow-left' size={20} color={'#FFF'}/>                    
+                </TouchableOpacity>
+            </View>
+            
+            <View style={styles.containerFavorite}>
+                <View style={styles.favorite}>
+                
+                <Text style={styles.favoriteProperty}>Título:</Text>
+                <Text style={styles.favoriteValue}>{favorite.title}</Text>
+
+                <Text style={styles.favoriteProperty}>Descrição:</Text>
+                <Text style={styles.favoriteValue}>{favorite.description}</Text>
+                
+                
+                <Text style={styles.favoriteProperty}>Link:</Text>
+                <TouchableOpacity onPress={()=>alert('WebView')}>
+                    <Text style={styles.favoriteValue}>{favorite.url}</Text>                
+                </TouchableOpacity>
+                
+                    <View style={styles.actions}>
+                        <TouchableOpacity style={styles.button} onPress={navigateToEdit}>
+                            <Text style={styles.buttonText}>Editar</Text>
+                            <AntDesign style={styles.button} name='edit' size={20} color={'#FFF'}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => alert('Deletar.')}>
+                            <Text style={styles.buttonText}>Apagar</Text>
+                            <Feather style={styles.button}  name='trash-2' size={20} color={'#FFF'}/>
+                        </TouchableOpacity>          
+                    </View>       
+                </View>       
+            </View>
         </View>
     );
 }
+
+/*
+                    
+                    URL com webView ----- JShunt
+                    Icone para imagem ----- ImageShare
+                    Método Apagar ------ ToDo
+                    Criar implementação do criar favorito na Página Create ------- ToDo
+                    Criar implementação do atualizar favorito na Página Update -------- ToDO
+*/
--- frontend/src/pages/Detail/styles.js
+++ frontend/src/pages/Detail/styles.js
@@ -5,14 +5,59 @@ export default StyleSheet.create({
 container: {
     flex: 1,
     backgroundColor: '#6c4fbb',
-    paddingTop: Constants.statusBarHeight + 10,      
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight + 10,            
   },
 
-  text:{
-    fontSize: 20,
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  return: {
+    flexDirection: 'row',
+    paddingHorizontal: 24,
+    marginTop: 30,
+  },
+
+  
+  containerFavorite:{    
+    paddingHorizontal: 20,
+  },
+
+  favorite:{
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#41414d',
+    marginTop: 48,        
+  },
+
+  favoriteProperty:{
+    fontSize: 18,
     color: '#FFF',
     fontWeight: 'bold',
+    marginTop: 24,
+  },
+
+  favoriteValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#FFF',
+  },
+  
+  actions:{
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  button:{
+    marginHorizontal:10,
+  },
+
+  buttonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#FFF',
   },
 });

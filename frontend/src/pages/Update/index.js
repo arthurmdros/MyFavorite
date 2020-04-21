37f04a7c1198bf4cef1310f95ccf7ad7f2d7ef75
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-21 00:14:49 -0300

    Adição de estilos e containers inicias na página Update

--- frontend/src/pages/Update/index.js
+++ frontend/src/pages/Update/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity, Image, View, Text} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import logoImg from '../../assets/logo_2X.png';
 import styles from './styles';
 
-export default function Create(){
+export default function Update(){
+    const navigation = useNavigation();
+
+    function navigateToBack(){
+        navigation.goBack();
+    }
+
     return(
         <View style={styles.container}>
-            <Text style={styles.text}>Update</Text>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <TouchableOpacity style={styles.return} onPress={navigateToBack}>
+                    <Feather name='arrow-left' size={20} color={'#FFF'}/>                    
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.headerTitle}>                
+                <Text style={styles.description}>
+                    Insira as informações abaixo:
+                </Text>
+            </View>
+
+            <View style={styles.newFavorite}>
+                <Text style={styles.text}> Formulario </Text>
+            </View>
+            
         </View>
     );
 }
--- frontend/src/pages/Update/styles.js
+++ frontend/src/pages/Update/styles.js
@@ -5,9 +5,30 @@ export default StyleSheet.create({
 container: {
     flex: 1,
     backgroundColor: '#6c4fbb',
-    paddingTop: Constants.statusBarHeight + 10,      
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight + 10,          
+  },
+  
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  return: {
+    flexDirection: 'row',
+    paddingHorizontal: 24,
+    marginTop: 30,
+  },
+  
+  description:{
+    fontSize: 16,    
+    marginBottom: 16,
+    marginTop: 20,
+    lineHeight: 24,
+    color: '#FFF'
+  },
+
+  headerTitle:{    
+    marginLeft: 20
   },
 
   text:{
@@ -15,4 +36,11 @@ container: {
     color: '#FFF',
     fontWeight: 'bold',
   },
+
+  newFavorite:{
+    marginTop: 10,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
 });

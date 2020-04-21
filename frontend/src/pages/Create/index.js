e9767df9d055ac8fed9a9ce569d1464388b7ad58
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-21 00:13:45 -0300

    Adição de estilos e containers inicias na página Create

--- frontend/src/pages/Create/index.js
+++ frontend/src/pages/Create/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity, Image, View, Text} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import logoImg from '../../assets/logo_2X.png';
 import styles from './styles';
 
 export default function Create(){
+    const navigation = useNavigation();
+
+    function navigateToMain(){
+        navigation.navigate('Main');
+    }
+
     return(
         <View style={styles.container}>
-            <Text style={styles.text}>Create</Text>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <TouchableOpacity style={styles.return} onPress={navigateToMain}>
+                    <Feather name='arrow-left' size={20} color={'#FFF'}/>                    
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.headerTitle}>                
+                <Text style={styles.description}>
+                    Insira as informações abaixo:
+                </Text>
+            </View>
+
+            <View style={styles.newFavorite}>
+                <Text style={styles.text}> Formulario </Text>
+            </View>
+            
         </View>
     );
 }
